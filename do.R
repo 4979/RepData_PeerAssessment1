4b921e76bc05e8b64be70e0eadcc9da5ee14efd2
Prabhat Kumar <4979@users.noreply.github.com>
2015-07-19 02:09:30 +0530

    initial do.R - commit.

---
+++ do.R
@@ -0,0 +1,9 @@
+# Reproducible Research: Course Project
+# Johns Hopkins University - Data Science Specialization.
+# Project Evaluation: (25% of final grade)
+# Course Project    : Activity Monitoring Devices Data
+# ============================================================
+# @author		: Prabhat Kumar
+# @date 		: 18-July-2015
+# @copyright: Prabhat Kumar
+# ============================================================
